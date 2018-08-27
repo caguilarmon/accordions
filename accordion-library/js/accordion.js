95d254cfca99f37f8ca9c0bc2de120f7e80e0e8b
Carlos Mac <macbookpro@MacBook-Pro-de-MacBook.local>
2018-08-26 21:25:12 -0600

    added transition speed setting

--- accordion-library/js/accordion.js
+++ accordion-library/js/accordion.js
@@ -28,29 +28,26 @@ var accordion = function (settings) {
 
     var moduleMaxHeight = getModuleMaxHeight(i);
 
-    // Expand Module    
-    if (moduleMaxHeight === 0){ 
+    // Expand Module
+    if (moduleMaxHeight === 0){
+      setTransitionSpeedIfAdded(i);
       accordionModule[i].style.maxHeight = accordionModule[i].scrollHeight + 'px';
       accordionModule[i].addEventListener('transitionend', setOverflow, false);
-
       // Set overflow to auto at the end of the expand, to add a scroll if the window is resized
       function setOverflow() {
         if(getModuleMaxHeight(i) !== 0){
           accordionModule[i].style.overflow = 'auto';
         }
       }
-
     // Collapse Module
     } else {
       accordionModule[i].style.maxHeight = 0;
       accordionModule[i].style.overflow = 'hidden';
-
     }
 
-    function getModuleMaxHeight(moduleIndex) {
-      return parseFloat(window.getComputedStyle(accordionModule[moduleIndex], null).getPropertyValue('max-height'));
+    function getModuleMaxHeight(i) {
+      return parseFloat(window.getComputedStyle(accordionModule[i], null).getPropertyValue('max-height'));
     }
-
   }
 
   // -------------------- No animation Accordion -------------------------------- //
@@ -67,7 +64,16 @@ var accordion = function (settings) {
     elem.classList.add(accordionClass);
   }
 
+  function isValueNumeric(val) {
+    return Number(val) === val;
+  };
+
+  function setTransitionSpeedIfAdded(i) {
+    if (isValueNumeric(settings.speed)) {
+      accordionModule[i].style.transitionDuration = settings.speed + 's';
+    }
+  }
+
 };
 
-accordion({accordion__btn:'accordion__btn', accordion__module:'accordion__module', type:'animated'});
-
+accordion({accordion__btn:'accordion__btn', accordion__module:'accordion__module', type:'animated', speed:.5});
