cceab75e87d24fae577b7748c37359257ffefa52
Carlos Mac <macbookpro@MacBook-Pro-de-MacBook.local>
2018-09-05 00:14:44 -0600

    added toggleOnOpen functionality for modules to auto collapse when a new one opens

--- accordion-library/js/accordion.js
+++ accordion-library/js/accordion.js
@@ -1,10 +1,10 @@
 var accordion = function (settings) {
 
-  // --------------------  Variables -------------------------------- //
+  // -------------------- Variables ----------------------------------------- //
   var accordionBtns = document.getElementsByClassName(settings.accordion__btn);
   var accordionModules = document.getElementsByClassName(settings.accordion__module);
 
-  // -------------------- Helper Functions -------------------------------- //
+  // -------------------- Helper Functions ---------------------------------- //
   var addClass = function(elem, accordionClass) {
     for (var i = 0; i < accordionClass.length; i++) {
       elem.classList.add(accordionClass[i]);
@@ -18,7 +18,7 @@ var accordion = function (settings) {
   // var toggleClass = function(elem, accordionClass) {
   //   elem.classList.toggle(accordionClass);
   // }
-  //
+
   // var containsClass = function(elem, accordionClass) {
   //   return elem.classList.contains(accordionClass);
   // }
@@ -27,9 +27,27 @@ var accordion = function (settings) {
     return Number(val) === val;
   };
 
-  var setTransitionSpeedIfAdded = function(elem) {
-    if (isValueNumeric(settings.speed)) {
+  var setTransitionSpeed = function(elem) {
       elem.style.transitionDuration = settings.speed + 's';
+  }
+
+  var getModuleMaxHeight = function(accordionMod) {
+    return parseFloat(window.getComputedStyle(accordionMod, null).getPropertyValue('max-height'));
+  }
+
+  var collapseModule = function(accordionMod) {
+    accordionMod.style.maxHeight = 0;
+    removeClass(accordionMod, 'accordion__module--animated-expanded');
+    addClass(accordionMod, ['accordion__module--animated-collapsed']);
+  }
+
+  var checkAndCollapseOtherExpandedModules = function(accordionMod) {
+    for (var i = 0; i < accordionModules.length; i++) {
+      if (!(accordionModules[i] === accordionMod)) {
+        if (getModuleMaxHeight(accordionModules[i]) !== 0) {
+          collapseModule(accordionModules[i]);
+        }
+      }
     }
   }
 
@@ -40,12 +58,17 @@ var accordion = function (settings) {
 
     // Expand Module
     if (moduleMaxHeight === 0){
-      setTransitionSpeedIfAdded(accordionMod);
+      if (settings.toggleOnOpen) {
+        checkAndCollapseOtherExpandedModules(accordionMod);
+      }
+      if (isValueNumeric(settings.speed)) {
+        setTransitionSpeed(accordionMod);
+      }
       accordionMod.style.maxHeight = accordionMod.scrollHeight + 'px';
-      accordionMod.addEventListener('transitionend', setOverflow, false);
+      accordionMod.addEventListener('transitionend', isModuleExpanded, false);
       // Set overflow to auto at the end of the expand,
       // to add a scroll if the window is resized
-      function setOverflow() {
+      function isModuleExpanded() {
         if(getModuleMaxHeight(accordionMod) !== 0){
           removeClass(accordionMod, 'accordion__module--animated-collapsed');
           addClass(accordionMod, ['accordion__module--animated-expanded']);
@@ -53,17 +76,11 @@ var accordion = function (settings) {
       }
     // Collapse Module
     } else {
-      accordionMod.style.maxHeight = 0;
-      removeClass(accordionMod, 'accordion__module--animated-expanded');
-      addClass(accordionMod, ['accordion__module--animated-collapsed']);
-    }
-
-    function getModuleMaxHeight(accordionMod) {
-      return parseFloat(window.getComputedStyle(accordionMod, null).getPropertyValue('max-height'));
+      collapseModule(accordionMod);
     }
   }
 
-  // -------------------- No animation Accordion -------------------------------- //
+  // -------------------- No animation Accordion ---------------------------- //
   var moduleHandlerNoAnimation = function(accordionMod) {
 
     var displayValue = window.getComputedStyle(accordionMod, null).getPropertyValue('display');
@@ -77,7 +94,7 @@ var accordion = function (settings) {
     }
   }
 
-  // -------------------- Type Handler -------------------------------- //
+  // -------------------- Type Handler -------------------------------------- //
   var typeHandler = function(i) {
     switch(settings.type) {
       case 'animated':
@@ -93,11 +110,11 @@ var accordion = function (settings) {
     }
   }
 
-  // -------------------- Init loop -------------------------------- //
+  // -------------------- Init loop ----------------------------------------- //
   for (var i = 0; i < accordionBtns.length; i++) {
       typeHandler(i);
   }
 
 };
 
-accordion({accordion__btn:'accordion__btn', accordion__module:'accordion__module', type:'animated', speed:.6});
+accordion({accordion__btn:'accordion__btn', accordion__module:'accordion__module', type:'animated', speed:.6, toggleOnOpen:true});
